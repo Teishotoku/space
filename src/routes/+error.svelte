45b09265a364a209e12fc1d099a684bd6164c9a4
Teishotoku <Dl1SqP1L4F0he@protonmail.com>
2025-03-04 23:46:25 +0200

    layout & error route

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<section>
+	<div class="errorBlock">
+		<h1>Страница</h1>
+		<h1>не найдена</h1>
+	</div>
+</section>
+
+<style lang="scss">
+	@use 'scss' as *;
+	section {
+		@include center;
+		width: 100vw;
+		height: 60vh;
+	}
+	.errorBlock {
+		@include start;
+		@include imgback('img/404.png');
+		width: 90%;
+		height: 90%;
+		color: $white;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,8 @@
 	import SectionMain from 'sections/sectionMain.svelte';
 </script>
 
-<main>
-	<SectionMain />
-</main>
+<SectionMain />
 
 <style lang="scss">
 	@use 'scss' as *;
-	main {
-		width: 100vw;
-		height: 100vh;
-		background-color: $prim;
-		@include columnAround;
-	}
 </style>
