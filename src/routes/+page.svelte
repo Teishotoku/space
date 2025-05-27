ed673955c9d635348f6d41de7073a344a64667cb
Teishotoku <Dl1SqP1L4F0he@protonmail.com>
2025-05-27 22:48:43 +0200

    section stack start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import SectionMain from 'sections/main/sectionMain.svelte';
+	import SectionAbout from 'sections/main/sectionAbout.svelte';
+	import SectionStack from '../ui/sections/main/sectionStack.svelte';
 </script>
 
 <SectionMain />
+<SectionAbout />
+<SectionStack />
 
 <style lang="scss">
 	@use 'scss' as *;
---
+++ src/ui/sections/main/sectionStack.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import NamingBlock from 'blocks/namingBlock.svelte';
+	import Tailwind from 'img/logo/sass.svg';
+	import Nest from 'img/logo/nest.svg';
+	import Fastapi from 'img/logo/fastapi.svg';
+	import Docker from 'img/logo/docker.svg';
+	import Laravel from 'img/logo/laravel.svg';
+	import Node from 'img/logo/nodejs.svg';
+</script>
+
+<section>
+	<div class="content">
+		<NamingBlock txt="stack" />
+		<div class="left">
+			<code lang="javascript">let a = 5</code>
+		</div>
+	</div>
+	<div class="right">
+		<div class="line">
+			<img src={Tailwind} alt="" />
+			<img src={Nest} alt="" />
+			<img src={Fastapi} alt="" />
+			<img src={Docker} alt="" />
+			<img src={Laravel} alt="" />
+			<img src={Node} alt="" />
+		</div>
+		<hr />
+	</div>
+</section>
+
+<style lang="scss">
+	@use 'scss' as *;
+	section {
+		@include section;
+		@include between;
+		background: linear-gradient(330deg, $black 35%, $dim 100%);
+	}
+	.content {
+		@include columnEvenly;
+		width: 65%;
+		height: 100%;
+	}
+	.left {
+		width: 90%;
+		height: 80%;
+		margin-left: 2%;
+		background: $dim;
+		border: 4px solid $red;
+	}
+	.right {
+		@include column;
+		width: 30%;
+		height: 100%;
+		hr {
+			width: 100%;
+			height: 16px;
+			color: $red;
+			background: $red;
+			border-radius: $rdLit 0 0 $rdBig;
+			&:hover {
+			}
+		}
+	}
+	.line {
+		@include columnEvenly;
+		background-color: $black;
+		position: relative;
+		overflow: auto;
+		width: 40%;
+		height: 100%;
+		border-inline: 2px solid $prim;
+		img {
+			width: 50%;
+			aspect-ratio: 1 / 1;
+			transition: all 0.5s;
+			-webkit-filter: drop-shadow(5px 5px 5px #00000000);
+			filter: drop-shadow(5px 5px 5px #00000000);
+			&:hover {
+				width: 80%;
+				filter: drop-shadow(8px 8px 8px $orange);
+			}
+		}
+	}
+</style>
